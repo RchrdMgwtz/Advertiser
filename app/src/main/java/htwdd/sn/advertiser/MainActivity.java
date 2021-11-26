417114ceac4787490c4583df127a513902aa9b2d
Richard Mogwitz <richard.mogwitz@posteo.de>
2021-11-26 17:15:58 +0100

    feat: Started advertising implementation

--- app/src/main/java/htwdd/sn/advertiser/MainActivity.java
+++ app/src/main/java/htwdd/sn/advertiser/MainActivity.java
@@ -1,17 +1,81 @@
 package htwdd.sn.advertiser;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertisingSet;
+import android.bluetooth.le.AdvertisingSetCallback;
+import android.bluetooth.le.AdvertisingSetParameters;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        if( !BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
+            Log.e( "e", "Multiple advertisement not supported");
+        }
+
+        this.advertise();
+
+        while(true) {}
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void advertise() {
+        BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
+
+        AdvertisingSetParameters parameters = (new AdvertisingSetParameters.Builder())
+                .setConnectable(false)
+                .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
+                .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
+                .build();
+
+        ParcelUuid uuid = new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid)));
+
+        AdvertiseData data = (new AdvertiseData.Builder())
+                .setIncludeDeviceName(true)
+                .addServiceUuid(uuid)
+                .addServiceData(uuid, "Moin".getBytes(StandardCharsets.UTF_8))
+                .build();
+
+        AdvertisingSetCallback callback = new AdvertisingSetCallback() {
+            @Override
+            public void onAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, int status) {
+                Log.i("i", "onAdvertisingSetStarted(): txPower:" + txPower + " , status: "
+                        + status);
+            }
+
+            @Override
+            public void onAdvertisingDataSet(AdvertisingSet advertisingSet, int status) {
+                Log.i("i", "onAdvertisingDataSet() :status:" + status);
+            }
+
+            @Override
+            public void onScanResponseDataSet(AdvertisingSet advertisingSet, int status) {
+                Log.i("i", "onScanResponseDataSet(): status:" + status);
+            }
+
+            @Override
+            public void onAdvertisingSetStopped(AdvertisingSet advertisingSet) {
+                Log.i("i", "onAdvertisingSetStopped():");
+            }
+        };
+
+        advertiser.startAdvertisingSet(parameters, data, null, null, null, callback);
     }
 }
