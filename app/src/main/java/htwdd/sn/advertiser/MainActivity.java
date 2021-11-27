c87f3144585675ccf4e1166f23788c2544614859
Richard Mogwitz <richard.mogwitz@posteo.de>
2021-11-27 18:35:55 +0100

    fix: ble packets too large

--- app/src/main/java/htwdd/sn/advertiser/MainActivity.java
+++ app/src/main/java/htwdd/sn/advertiser/MainActivity.java
@@ -1,5 +1,6 @@
 package htwdd.sn.advertiser;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
@@ -9,14 +10,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -26,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         if( !BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
             Log.e( "BLE", "Multiple advertisement not supported");
+            return;
         }
+
+        this.textView = findViewById(R.id.textView);
 
         this.advertise();
     }
@@ -37,27 +43,28 @@ public class MainActivity extends AppCompatActivity {
 
         AdvertiseSettings settings = (new AdvertiseSettings.Builder())
                 .setConnectable(false)
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
                 .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
                 .build();
 
         ParcelUuid uuid = new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid)));
 
         AdvertiseData data = (new AdvertiseData.Builder())
-                .setIncludeDeviceName(true)
                 .addServiceUuid(uuid)
-                .addServiceData(uuid, "Moin".getBytes(StandardCharsets.UTF_8))
                 .build();
 
         AdvertiseCallback callback = new AdvertiseCallback() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                textView.setText(R.string.advertsing);
                 super.onStartSuccess(settingsInEffect);
             }
 
+            @SuppressLint("SetTextI18n")
             @Override
             public void onStartFailure(int errorCode) {
-                Log.e( "BLE", "Advertising onStartFailure: " + errorCode );
+                textView.setText(getString(R.string.start_failed) + errorCode);
                 super.onStartFailure(errorCode);
             }
         };
